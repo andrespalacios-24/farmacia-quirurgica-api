c5a1dde1ff8cea438b656448e35c5dda4d1cfa5d
ANDRES PALACIOS <andresfelipepalaciosviveros@gmail.com>
2026-08-17 01:58:47 -0500

    chore(models): eliminar modelos Pydantic legacy de MongoDB

--- app/models/common.py
+++
@@ -1,6 +0,0 @@
-# app/models/common.py
-from typing import Annotated
-from pydantic import BeforeValidator
-
-# Pydantic v2 Validator para convertir ObjectId de Mongo a str automáticamente
-PyObjectId = Annotated[str, BeforeValidator(lambda x: str(x) if x is not None else None)]
